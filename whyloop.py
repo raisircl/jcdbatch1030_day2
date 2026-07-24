14aadfea72f7275bb6d6f59344fb3861a7d91bca
raisircl <rai.verma@live.com>
2026-07-24 10:55:20 +0530

    loops

---
+++ whyloop.py
@@ -0,0 +1,41 @@
+# when you need to repeat a statement or set of statements again and again
+# we use the loops to do that. There are two types of loops in python
+# while loop and for loop
+# today we will learn about while loop
+# first of all we initialize a loop counter variable
+# loop_counter=value
+# while condition:
+#     statement(s)
+#     incr / decr loop_counter  
+
+i=1 # set the loop counter variable to 1
+while i<=5:
+    print(f"{i}. Hello User!")
+    i=i+1
+
+# Dry Run
+# i   condition   output
+# 1   1<=5 True   Hello User!
+# 2   2<=5 True   Hello User!
+# 3   3<=5 True   Hello User!
+# 4   4<=5 True   Hello User!
+# 5   5<=5 True   Hello User!
+# 6   6<=5 False
+
+# assignment print row number with hello user
+# 1. Hello User!
+# 2. Hello User!
+# 3. Hello User!
+# 4. Hello User!
+# 5. Hello User!
+# print 10 to 1 in reverse order
+# print table of 2
+# print 1, 4, 9, 16, 25, 36, 49, 64, 81, 100
+# print 1, 2, 4, 8, 16, 32, 64, 128, 256, 512
+# enter a number from user and print the table of that number
+# example: enter a number: 5
+# 5 x 1 = 5
+# 5 x 2 = 10
+# 5 x 3 = 15
+# Assignment: print the sum of first 10 natural numbers
+# Assignment: enter a number and print its factorial
