d77a6fca5d637baf28b9d724993781c9fcaf3883
raisircl <rai.verma@live.com>
2026-08-07 17:55:44 +0530

    bankacc

--- oops/person.py
+++ oops/person.py
@@ -1,10 +1,12 @@
 class person:
     count=0 # class attribute
     def __init__(self, name="unknown", age=0):
+        person.count += 1 # incrementing the class attribute
+        self.uuid=person.count # instance attribute
         self.name=name # instance attribute
         self.age=age # instance attribute
     def display(self):
-        print(f"Name : {self.name} Age: {self.age}")
+        print(f"UUID: {self.uuid}, Name : {self.name} Age: {self.age}")
 
 p1=person()
 p1.name="John"
@@ -21,3 +23,10 @@ p2.display()
 # Box - l, b, h , methos - display, area, volume
 
 # Circle - r, methods - display, area, circumference
+
+# BankAccount - 
+# Instance attributes - accno, name, balance
+# methods - display, deposit, withdraw
+# accno should be auto generated and start from 1001
+# take a class attrribute x intialize it with 1000
+
